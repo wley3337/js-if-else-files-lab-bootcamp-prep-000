5552db8d745b8b791a31f9d2f4dbc295dfda3bc7
wley3337@gmail.com <wley3337@gmail.com>
2018-02-12 22:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var name = "Joe"
+var name = "Joe";
 var height = "74"
+
+if (name === "Susan"){
+  console.log("Your name is Susan!")
+} else { console.log("Your name is " + name)};
+
 
 
 // Don't worry about this
